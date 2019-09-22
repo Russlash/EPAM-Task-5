4106b2b3841938df7ee0eb58c33a6617f5ce488a
Руслан Гусейнов <guseinov.ruslan9@mail.ru>
2019-09-22 18:04:36 +0400

    Добавьте файлы проекта.

---
+++ Task 5/BracketsChecker.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class BracketsChecker
+    {
+        private readonly string _opening = "([{";
+        private readonly string _closing = ")]}";
+
+        private bool _cantBeBalanced;
+
+        private Stack<int> _opened = new Stack<int>();
+
+        public bool Balanced => !_cantBeBalanced && !_opened.Any();
+
+        public void Put(char ch)
+        {
+            if (_cantBeBalanced) return;
+
+            int index = _opening.IndexOf(ch);
+
+            if (index != -1)
+            {
+                _opened.Push(index);
+                return;
+            }
+
+            index = _closing.IndexOf(ch);
+
+            if (index != -1)
+            {
+                if (!_opened.Any() || _opened.Peek() != index)
+                {
+                    _cantBeBalanced = true;
+                    _opened.Clear();
+                    _opened.TrimExcess();
+                    return;
+                }
+
+                _opened.Pop();
+                return;
+            }
+        }
+    }
+}
---
+++ Task 5/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "[(]{})";
+            var checker = new BracketsChecker();
+
+            foreach (var ch in s)
+                checker.Put(ch);
+
+            Console.WriteLine(checker.Balanced);
+
+            var s1 = "()[()]{()()[]}";
+            var checker1 = new BracketsChecker();
+
+            foreach (var ch1 in s1)
+                checker.Put(ch1);
+
+            Console.WriteLine(checker1.Balanced);
+
+            Console.ReadLine();
+        }
+    }
+}
